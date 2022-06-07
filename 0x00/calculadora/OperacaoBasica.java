93fdc28c7392a624580e2c511280093c3f9fd640
gabrielamanoel <gabrielamanoel@ciandt.com>
2022-06-07 14:25:00 -0300

    g

---
+++ 0x00/calculadora/OperacaoBasica.java
@@ -0,0 +1,18 @@
+public class OperacaoBasica {
+
+  static Double somar(double a, double b){
+        return a + b;
+    }
+
+    static Double subtrair(double a, double b){
+        return a - b;
+    }
+
+    static Double multiplicar(double a, double b){
+        return a * b;
+    }
+
+    static Double dividir(double a, double b){
+        return a / b;
+    }
+}
